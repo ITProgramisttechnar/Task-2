878a767319697cf6cc80e008205003ad8034bf01
ITProgramisttechnar <samyro001@mail.ru>
2026-01-26 21:49:16 +0300

    Сделал задание

---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String nickname;
+
+    public void setNickname(String nickname){
+        this.nickname=nickname;
+    }
+    public String getNickname(){
+        return nickname;
+    }
+}
